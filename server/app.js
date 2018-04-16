f9e574eacbd47720d33a9ac406d3de909433a144
DanneG1 <danne-g@hotmail.com>
2018-04-16 12:11:49 +0200

    server app.js update

--- server/app.js
+++ server/app.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static('../'));
 app.use(express.static('../'))
-app.get('/get_test',function(req,resp){
+app.get('/app_get', function (req, resp) {
     console.log("GET request");
     resp.send('Nothing to GET');
 });
-app.post('/post_test',function(req,resp){
+app.post('/app_post', function (req, resp) {
     console.log("POST request");
     resp.send('Nothing to post yet');
 });
 
-var server = app.listen(7000,function(){
-    var host=server.address().address;
-    var port=server.address().port;
-    console.log('Express app listening at http://%s:%s',host,port);
+app.delete('/app_delete', function (req, resp) {
+    console.log("Delete request");
+    resp.send("Delete request sent");
+});
+
+var server = app.listen(80, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Express app listening at http://%s:%s', host, port);
+});
+
+server.on('connection',conn=>{
+    console.log("Connection recieved");
 });
