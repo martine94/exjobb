d4c5df73de808043c56e60911092674172e7a0fd
DanneG1 <danne-g@hotmail.com>
2018-04-16 13:50:30 +0200

    method taggar i index.html, fix i server app.js, samt massa testkod i server/copyPlattform

--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser=require('body-parser');
+var urlEncodedParcer=bodyParser.urlencoded({extended:false});
 
-app.use(express.static('../'))
+
+//Ändra till express.static('../') för att nå riktiga sidan
+//app.use(express.static('./copyPlattform/'))
+app.use(express.static('../'));
 app.get('/app_get', function (req, resp) {
     console.log("GET request");
     resp.send('Nothing to GET');
 });
-app.post('/app_post', function (req, resp) {
+app.post('/register_company.html',urlEncodedParcer, function (req, resp) {
     console.log("POST request");
-    resp.send('Nothing to post yet');
+    response={
+        //format: [variabelnamn]:req.body.[inmatningsfönstrets namn]
+        //first_name:req.body.first_name,
+        //last_name:req.body.last_name
+        cname:req.body.cname,
+        caddress:req.body.cadress,
+        ccity:req.body.ccity
+    };
+    console.log(response)
+    //Fixa så man inte kommer åt helvete efter submit
+    resp.end();
 });
 
 app.delete('/app_delete', function (req, resp) {
