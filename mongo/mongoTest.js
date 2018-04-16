8a0a39d45157e4ee4687a4680f0e05e15af75a5c
JHenriksson86 <j.henriksson86@gmail.com>
2018-04-16 14:56:25 +0200

    Databas funktioner tillaggda...

--- mongo/mongoTest.js
+++ mongo/mongoTest.js
@@ -1,28 +1,70 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = [
-    { name: 'John', address: 'Highway 71'},
-    { name: 'Peter', address: 'Lowstreet 4'},
-    { name: 'Amy', address: 'Apple st 652'},
-    { name: 'Hannah', address: 'Mountain 21'},
-    { name: 'Michael', address: 'Valley 345'},
-    { name: 'Sandy', address: 'Ocean blvd 2'},
-    { name: 'Betty', address: 'Green Grass 1'},
-    { name: 'Richard', address: 'Sky st 331'},
-    { name: 'Susan', address: 'One way 98'},
-    { name: 'Vicky', address: 'Yellow Garden 2'},
-    { name: 'Ben', address: 'Park Lane 38'},
-    { name: 'William', address: 'Central st 954'},
-    { name: 'Chuck', address: 'Main Road 989'},
-    { name: 'Viola', address: 'Sideway 1633'}
-  ];
-  dbo.collection("customers").insertMany(myobj, function(err, res) {
+let mystudents = [
+  { _user: 'a', password: 'a'},
+  { _user: 'b', password: 'b'},
+  { _user: 'c', password: 'c'},
+  { _user: 'd', password: 'd'},
+  { _user: 'e', password: 'e'},
+  { _user: 'f', password: 'f'},
+  { _user: 'g', password: 'g'},
+  { _user: 'h', password: 'h'},
+  { _user: 'i', password: 'i'},
+  { _user: 'j', password: 'j'}
+];
+
+function addStudent(student)
+{
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("Number of documents inserted: " + res.insertedCount);
-    db.close();
+    let dbo = db.db("db");
+
+    dbo.collection("student").ensureIndex({_user: 1}, {unique : true});
+
+    dbo.collection("student").insertOne(student, function(err, res) {
+      if (err) {
+        if(err.code === 11000)
+        {
+          console.log("Användarnamnet är upptaget!");
+          return false;
+        }
+        else{
+          console.log(err);
+          throw err;
+        }
+      }
+      console.log("Number of documents inserted: " + res.insertedCount);
+      db.close();
+
+      if(res.insertedCount === 1)
+        return true;
+      else
+        return false;
+    });
   });
-});
+}
+
+function loginStudent(usr, psw)
+{
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    let dbo = db.db("db");
+    let query = { _user: usr, password: psw };
+   
+    dbo.collection("student").find(query).toArray(function(err, result) {
+      if (err) throw err;
+     
+      console.log(result.length);
+      db.close();
+     
+      if(result.length === 1)
+        return true;
+      else
+        return false;
+    });
+  });
+}
+
+addStudent({ _user: 'y', password: 'y'});
+
