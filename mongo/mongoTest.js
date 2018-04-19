39d11a839020e98b6c792e69481eb363a5e565cc
JHenriksson86 <j.henriksson86@gmail.com>
2018-04-19 15:37:37 +0200

    Cleaning mongoTest.js

--- mongo/mongoTest.js
+++ mongo/mongoTest.js
@@ -2,53 +2,31 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var database = "db";
 
-let myaccounts = [
-  { _user: 'a', password: 'a'},
-  { _user: 'b', password: 'b'},
-  { _user: 'c', password: 'c'},
-  { _user: 'd', password: 'd'},
-  { _user: 'e', password: 'e'},
-  { _user: 'f', password: 'f'},
-  { _user: 'g', password: 'g'},
-  { _user: 'h', password: 'h'},
-  { _user: 'i', password: 'i'},
-  { _user: 'j', password: 'j'}
-];
-
 module.exports = {
 
-login : function(table, usr, psw, callback){
-  
-  MongoClient.connect(url, async function(err, db) {
-    if (err) throw err;
+  login : function(table, usr, psw, callback){
     
-    console.log("Connected to database!");
-
-    let dbo = db.db(database);
-    let query = { _user: usr, password: psw };
-    
-    dbo.collection(table).find(query).toArray(function(err, result) {
+    MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-
-      console.log("Entered collection success!");
       
-      db.close();
+      console.log("Connected to database!");
 
-      callback(result);
+      let dbo = db.db(database);
+      let query = { _user: usr, password: psw };
+      
+      dbo.collection(table).find(query).toArray(function(err, result) {
+        if (err) throw err;
+
+        console.log("Entered collection success!");
+        
+        db.close();
+
+        callback(result);
+      });
     });
- });
-}
-
+  }
 
 };
-
-function createTestDb(content, func)
-{
-  for(stuff in content)
-  {
-    func(content[stuff]);
-  }
-}
 
 function addStudent(student)
 {
@@ -98,8 +76,32 @@ function addCompany(company)
   });
 }
 
-//createTestDb(myaccounts, addCompany);
+//Array med test konton.
 
-// login("student", "b", "b", function(result){
-//   console.log("Callback from callback function: " + result);
-// });
+var myaccounts = [
+  { _user: 'a', password: 'a'},
+  { _user: 'b', password: 'b'},
+  { _user: 'c', password: 'c'},
+  { _user: 'd', password: 'd'},
+  { _user: 'e', password: 'e'},
+  { _user: 'f', password: 'f'},
+  { _user: 'g', password: 'g'},
+  { _user: 'h', password: 'h'},
+  { _user: 'i', password: 'i'},
+  { _user: 'j', password: 'j'}
+];
+
+//Function to populate db 
+
+function createTestDb(content, func)
+{
+  for(stuff in content)
+  {
+    func(content[stuff]);
+  }
+}
+
+//Uncomment to populate db.
+
+//createTestDb(myaccounts, addCompany);
+//createTestDb(myaccounts, addStudent);
