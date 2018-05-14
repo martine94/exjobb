f07c9739d803718833ea8b9acb6808c241c86a13
martine94 <M.E-94@hotmail.com>
2018-05-14 08:31:14 +0200

    ändrade så det står kort meddelande för företag

--- script/Company.js
+++ script/Company.js
@@ -332,7 +332,7 @@ window.onload = function () {
             let divInfo = document.createElement("div");
             let shortDescript = document.createElement("p");
             shortDescript.style.outlineColor = "brown";
-            shortDescript.innerHTML = "Kort beskrivning om sökanden: ";
+            shortDescript.innerHTML = "Kort meddelande: ";
             divInfo.appendChild(shortDescript);
             personDiv.appendChild(divInfo);
             
